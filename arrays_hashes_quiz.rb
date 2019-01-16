9102c493c339fc4b1b00e344259f18d8ae35c70e
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-16 21:52:27 +0000

    Removing specific item from array

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -20,8 +20,9 @@ stops.insert(4, "Polmont")
 p stops
 # 4. Work out the index position of `"Linlithgow"
 p stops.find_index("Linlithgow")
-
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
+p stops
 # 6. Delete `"Cumbernauld"` from the array by index
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
