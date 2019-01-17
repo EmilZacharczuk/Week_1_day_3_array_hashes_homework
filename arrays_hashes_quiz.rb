b85732d12326433b116d53dbf1e663bd486ccd4d
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-17 07:05:00 +0000

    Looping through array and printing keys of individual hashes

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -159,5 +159,15 @@ united_kingdom = [
 united_kingdom[1][:capital] = "Cardiff"
 p united_kingdom[1]
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+northern_ireland = {
+  name: "Northern Ireland",
+  population: 1811000,
+  capital: "Belfast"
+}
+united_kingdom.push(northern_ireland)
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
